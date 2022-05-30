5c8c93a4aa0ff787f82261a2978e2ce41ae2ae56
santiSz3 <santiago@antic.xyz>
2022-05-31 01:03:27 +0300

    test

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/santiago/Desktop/University/OS/Ex3
+# Install script for directory: /Users/santiago/Desktop/University/OS/ex3OS
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/santiago/Desktop/University/OS/Ex3/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/santiago/Desktop/University/OS/ex3OS/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
